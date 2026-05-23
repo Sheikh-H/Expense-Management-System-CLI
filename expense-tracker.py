20e55051800bd59bc7271828dfc6a6c13219f5d1
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-24 00:06:26 +0100

    started next project on the roadmap.sh list

--- expense-tracker.py
+++ expense-tracker.py
@@ -3,14 +3,12 @@ import sys
 import datetime
 import csv
 
+# Since I have been using the json file for storing data for the last two tasks, I would like to try and use a csv file. I used it a little during the CS50x projects but wasn't that proficient and would like to get myself a little familiar with using this format a little more.
 
-with open("expenses.csv", 'w') as f:
-    csv.DictWriter(f,['field1','field2'])
-    with open("expenses.csv", 'r') as f:
-        data = csv.DictReader(f)
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
